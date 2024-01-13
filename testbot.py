399b6514d39765282e4c779390cd158394b033b6
Vyuu <91697411+waroo19@users.noreply.github.com>
2024-01-13 05:51:09 +0100

    Add files via upload
    new bot in use with pythontelebot

---
+++ testbot.py
@@ -0,0 +1,69 @@
+import logging
+import threading
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# Set of subscribed topics
+subscribed_topics = set()
+
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+BOT_TOKEN = "6881742840:AAEgTkuFUXNucJdSejI8ZfZ5e0OCzUleqtQ"
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hello! I am your MQTT bot.')
+
+def stop_bot():
+    updater.stop()
+    updater.is_idle = False  # Terminate the updater's idle loop
+
+def stop(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Stopping the bot...')
+    threading.Thread(target=stop_bot).start()
+
+#Function to subscribe to an MQTT topics
+def subscribe(update: Update, context: CallbackContext) -> None:
+
+    #Get chat ID and the topic from the user's message
+    chat_id = update.message.chat.id
+    command_parts = update.message.text.split(' ', 1)
+
+    # Check if the command is "/subscribe" and there is a topic provided
+    if len(command_parts) > 1 and command_parts[0].lower() == "/subscribe":
+        topic = command_parts[1].strip()
+        # Now 'topic' contains the extracted topic
+        update.message.reply_text(f"Subscribing to topic: {topic}")
+    else:
+        update.message.reply_text("Invalid /subscribe command. Please provide a topic.")
+
+    # Add the topic to the set of subscribed topics
+    subscribed_topics.add(topic)
+
+    # Inform the user about the subscription
+    update.message.reply_text(f"Subscribed MQTT topics: {topic}")
+
+
+
+
+def main() -> None:
+    global updater
+    # Create the Updater and pass it the bot's token
+    updater = Updater(BOT_TOKEN)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Register the  commands
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("stop", stop))
+    dp.add_handler(CommandHandler("subscribe", subscribe))
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you send a signal to stop it
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
