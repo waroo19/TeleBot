83123df06a4355251e2482cad3f0f2659a7828e0
Vyuu <91697411+waroo19@users.noreply.github.com>
2024-04-07 13:39:24 +0200

    work with onmsg

--- testingonmsg.py
+++ testingonmsg.py
@@ -1,16 +1,19 @@
 # python 3.11
 
 import random
-
+import threading
 from paho.mqtt import client as mqtt_client
 from telegram.ext import Updater, CommandHandler
 import logging
-
+from telegram import Update
+from telegram.ext import CallbackContext
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+# Telegram bot token
+BOT_TOKEN = "6881742840:AAEgTkuFUXNucJdSejI8ZfZ5e0OCzUleqtQ"
 
 broker = '18.212.148.133'
 port = 1883
-topic = "testtopic"
+topic = "testing"
 # Generate a Client ID with the subscribe prefix.
 client_id = f'subscribe-{random.randint(0, 100)}'
 
@@ -23,11 +26,9 @@ def connect_mqtt() -> mqtt_client:
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
-
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
@@ -37,22 +38,37 @@ def subscribe(client: mqtt_client):
     client.on_message = on_message
 
 
+
 def connect_broker(update, context):
     """Connect to MQTT broker."""
-    try:
-        client = connect_mqtt()
-        update.message.reply_text("Connected to MQTT broker.")
-    except Exception as e:
-        update.message.reply_text(f"Failed to connect to MQTT broker: {e}")
-
-
-
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_start()
+    update.message.reply_text("Connected to MQTT broker.")
 
 def run():
     client = connect_mqtt()
     subscribe(client)
     client.loop_forever()
 
+def main():
+    
+    # Create the Updater and pass it the bot's token
+    updater = Updater(BOT_TOKEN, use_context=True)
+    
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
+    # Register the command handler for /connect
+    dp.add_handler(CommandHandler("con", connect_broker))
+    dp.add_handler(CommandHandler("help", help))
+
+    # Start the Bot
+    updater.start_polling()
+
+
+    # Run the bot until you send a signal to stop it
+    updater.idle()
+    
 if __name__ == '__main__':
-    run()
+    main()
