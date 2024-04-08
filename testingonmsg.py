11ed8c0c10c796a6de4d7d9c63d9528adfdebc87
Vyuu <91697411+waroo19@users.noreply.github.com>
2024-04-08 14:05:25 +0200

    Add files via upload

--- testingonmsg.py
+++ testingonmsg.py
@@ -1,7 +1,7 @@
 # python 3.11
 
 import random
-import threading
+import time
 from paho.mqtt import client as mqtt_client
 from telegram.ext import Updater, CommandHandler
 import logging
@@ -9,13 +9,13 @@ from telegram import Update
 from telegram.ext import CallbackContext
 from telegram import Bot
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 # Telegram bot token
 BOT_TOKEN = "6881742840:AAEgTkuFUXNucJdSejI8ZfZ5e0OCzUleqtQ"
 
 broker = '54.85.61.173'
 port = 1883
-topic = "testing"
+topics = ["topic1", "topic2", "topic3"]
 # Generate a Client ID with the subscribe prefix.
 client_id = f'subscribe-{random.randint(0, 100)}'
 
@@ -41,25 +41,37 @@ def on_message(client, userdata, msg):
     bot.send_message(chat_id=chat_id, text=message)
 
 
+def on_publish(client,userdata,mid):             #create function for callback
+    print(f"Data on published with mid value: {mid}")
 
 
-
-
+def publish(client,topic, msg):
+    client.on_publish = on_publish
+    client.publish(topic,msg)
 def subscribe_topic(client: mqtt_client):
-    
-
-    client.subscribe(topic)
+    for topic in topics:
+        client.subscribe(topic)
     client.on_message = on_message
-
 
 
 def connect_broker(update, context):
     """Connect to MQTT broker."""
     client = connect_mqtt()
+    context.bot_data['mqtt_broker'] = client
     subscribe_topic(client)
     client.loop_start()
     update.message.reply_text("Connected to MQTT broker.")
-    update.message.reply_text(f"Subscribed to topic: {topic}")
+    update.message.reply_text(f"Subscribed to following topics: {', '.join(topics)}")
+
+def publish_command(update, context):
+    client = context.bot_data.get('mqtt_broker') 
+    
+    topic = context.args[0]
+    msg = context.args[1]
+    publish(client, topic, msg)
+    update.message.reply_text("Messages published successfully.")
+    
+
 
 def start(update: Update, context: CallbackContext) -> None:
     global chat_id
@@ -81,6 +93,7 @@ def main():
     dp.add_handler(CommandHandler("con", connect_broker))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("pub", publish_command))
     # Start the Bot
     updater.start_polling()
 
