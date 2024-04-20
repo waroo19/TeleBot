b74638cd616483c811a0704b4980a7ce67550ed2
Vyuu <91697411+waroo19@users.noreply.github.com>
2024-04-20 17:46:01 +0200

    update welcome text

--- testingonmsg.py
+++ testingonmsg.py
@@ -3,21 +3,28 @@
 import random
 import time
 from paho.mqtt import client as mqtt_client
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 import logging
 from telegram import Update
 from telegram.ext import CallbackContext
 from telegram import Bot
+import requests
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # Telegram bot token
 BOT_TOKEN = "6881742840:AAEgTkuFUXNucJdSejI8ZfZ5e0OCzUleqtQ"
 
-broker = '54.85.61.173'
+broker = '3.87.254.59'
 port = 1883
-topics = ["topic1", "topic2", "topic3"]
+topics = ["sensors/temperature","sensors/motion", "sensors/light"]
 # Generate a Client ID with the subscribe prefix.
 client_id = f'subscribe-{random.randint(0, 100)}'
+
+
+
 
 
 def connect_mqtt() -> mqtt_client:
@@ -71,6 +78,28 @@ def publish_command(update, context):
     publish(client, topic, msg)
     update.message.reply_text("Messages published successfully.")
     
+def unknown_text(update,context):
+    welcome_message = """
+    Hi there!  I'm the Home Guardian bot, keeping an eye on your environment. 
+
+    Type these commands to get started:
+    /start - I hope you know what you do 
+    /help - See a list of all available commands.
+    """
+    update.message.reply_text(welcome_message)
+
+def help(update,context):
+    help_message = """
+    Hi there!  I'm the Home Guardian bot, keeping an eye on your environment. 
+
+    Here are all the available commands:
+    /help - See a list of all available commands.
+    /start - Warming up before you can connect to the broker
+    /con - Connect to the MQTT Broker
+    /pub topic/name message - Publish message to topic
+    """
+    update.message.reply_text(help_message)
+
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -79,21 +108,22 @@ def start(update: Update, context: CallbackContext) -> None:
     
     print(f"Chat ID is: {chat_id}")
     context.bot.send_message(chat_id=chat_id, text="Welcome")
-
+    
 
 def main():
     
     # Create the Updater and pass it the bot's token
-    updater = Updater(BOT_TOKEN, use_context=True)
+    updater = Updater(BOT_TOKEN,use_context=True)
     
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
-
+    
     # Register the command handler for /connect
     dp.add_handler(CommandHandler("con", connect_broker))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("pub", publish_command))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text)) 
     # Start the Bot
     updater.start_polling()
 
